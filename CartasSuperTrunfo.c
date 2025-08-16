aff6b164cde29d1d7a11b7e140952352b4d9ac00
Carlos080519 <202503561061@alunos.estacio.br>
2025-08-16 04:59:07 +0000

    teste 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
-
-
     int main(){
 
       /*variaveis definadas*/
-      char estado[50];
-      char codcarta[50];
-      char nomecity[50];
-      cha populacao[50];
-      float area_km;
-      float pib;
-      int turismo;
-                  /*Parte 1 cadastro de cartas*/
+        char estado[50];
+        char codcarta[50];
+        char nomecity[50];
+        int populacao;
+        double area_km;
+        double pib;
+        int turismo;
+
+      /*Parte 1 cadastro de cartas*/
                    printf("Cadastro de Cartas! \n");
 
-                  /*Ter mais linhas separadas*/
+                  /*Ter mais linhas puladas*/
         printf("\n");
         printf("\n");
 
@@ -29,20 +28,22 @@
                   printf("Cadastre uma Cidade: \n");
                     scanf("%s", nomecity);
 
-                  printf("Cadastre a População: \n");
-                    scanf("%s", populacao);
-
                   printf("Cadastre a Area em Km²: \n");
                     scanf("%f", area_km);
 
                   printf("Cadastre O PIB: \n");
                     scanf("%f", pib);   
+                    
+      
+        printf("\n");
+        printf("\n");
 
-                  printf("Cadastre um Ponto Turistico : \n");
-                    scanf("%i", turismo);
 
                     /*imprimir as cartas do estado 1*/
                     printf("Codigo da Carta : %s \n",codcarta );
                     printf("Estado : %s \n", estado);
-  return 0;    
+                    printf("Cidade : %s \n", nomecity);
+                    printf("Area em Km² : %.2f \n", area_km);
+                    printf("PIB : %.2f \n", pib);
+                      return 0;    
 }
