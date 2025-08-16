b8e8809410236fb4e18a40b7bc545f7abd1262fd
Carlos080519 <202503561061@alunos.estacio.br>
2025-08-16 05:37:29 +0000

    cadastro das cartas no super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,25 @@
 
       /*variaveis definadas*/
         char estado[50];
+        char estado2[50];
+
         char codcarta[50];
+        char codcarta2[50];
+        
         char nomecity[50];
+        char nomecity2[50];
+
         int populacao;
-        double area_km;
-        double pib;
+        int populacao2;
+
+        float area_km;
+        float area_km2;
+        
+        float pib;
+        float pib2;
+        
         int turismo;
+        int turismo2;
 
       /*Parte 1 cadastro de cartas*/
                    printf("Cadastro de Cartas! \n");
@@ -29,21 +42,87 @@
                     scanf("%s", nomecity);
 
                   printf("Cadastre a Area em Km²: \n");
-                    scanf("%f", area_km);
+                    scanf("%f", &area_km);
 
                   printf("Cadastre O PIB: \n");
-                    scanf("%f", pib);   
-                    
-      
+                    scanf("%f", &pib);   
+                  
+                  printf("Cadastre a Pulação: \n");
+                    scanf("%d", &populacao);
+                  
+                  printf("Cadastre o Turismo: \n"); 
+                    scanf("%d", &turismo);
+
+                  
+        /*Pular linha*/
         printf("\n");
-        printf("\n");
+    
+        /*imprimir as cartas do estado 1*/
+                    printf("Codigo da Carta: %s \n",codcarta );
+                      printf("\n");
+                    printf("Estado: %s \n", estado);
+                      printf("\n");
+                    printf("Cidade: %s \n", nomecity);
+                      printf("\n");
+                    printf("Area em Km²: %.2f \n", area_km);
+                      printf("\n");
+                    printf("PIB: %.2f \n", pib);
+                      printf("\n");
+                    printf("Polução: %i \n", populacao);
+                      printf("\n");
+                    printf("Turismo: %i \n", turismo);      
+                      printf("\n");
+
+              /*Parte 2 cadastro de cartas*/
+              
+                    printf("Cadastro de Cartas 2 ! \n");
+
+               /*Ter mais linhas puladas*/
+                    printf("\n");
+                    printf("\n");
 
 
-                    /*imprimir as cartas do estado 1*/
-                    printf("Codigo da Carta : %s \n",codcarta );
-                    printf("Estado : %s \n", estado);
-                    printf("Cidade : %s \n", nomecity);
-                    printf("Area em Km² : %.2f \n", area_km);
-                    printf("PIB : %.2f \n", pib);
-                      return 0;    
+                  printf("Cadastre um Codigo para a Carta: \n");
+                    scanf("%s", codcarta2);
+                  
+                  printf("Cadastre um Estado: \n");
+                    scanf("%s", estado2);
+                  
+                  printf("Cadastre uma Cidade: \n");
+                    scanf("%s", nomecity2);
+
+                  printf("Cadastre a Area em Km²: \n");
+                    scanf("%f", &area_km2);
+
+                  printf("Cadastre O PIB: \n");
+                    scanf("%f", &pib2);   
+                  
+                  printf("Cadastre a Pulação: \n");
+                    scanf("%d", &populacao2);
+                  
+                  printf("Cadastre o Turismo: \n"); 
+                    scanf("%d", &turismo2);
+
+                  
+        /*Pular linha*/
+        printf("\n");
+    
+        /*imprimir as cartas do estado 1*/
+                    printf("Codigo da Carta: %s \n",codcarta2 );
+                      printf("\n");
+                    printf("Estado: %s \n", estado2);
+                      printf("\n");
+                    printf("Cidade: %s \n", nomecity2);
+                      printf("\n");
+                    printf("Area em Km²: %.2f \n", area_km2);
+                      printf("\n");
+                    printf("PIB: %.2f \n", pib2);
+                      printf("\n");
+                    printf("Polução: %i \n", populacao2);
+                      printf("\n");
+                    printf("Turismo: %i \n", turismo2);      
+                      printf("\n");
+
+
+  return 0;    
 }
